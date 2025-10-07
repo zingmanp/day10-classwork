ad81d2afa8ab957446a608041ba4309087aa0550
Sadiq Abdulrahaman <sadiqabdulrahman00880@gmail.com>
2025-10-07 16:38:28 +0100

    app test file update

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Full Stack App heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/Full Stack App/i);
+  expect(headingElement).toBeInTheDocument();
+});
+
+test('renders Add User button', () => {
+  render(<App />);
+  const buttonElement = screen.getByText(/Add User/i);
+  expect(buttonElement).toBeInTheDocument();
+});
+
+test('renders Users heading', () => {
+  render(<App />);
+  const usersHeading = screen.getByText(/Users/i);
+  expect(usersHeading).toBeInTheDocument();
 });
